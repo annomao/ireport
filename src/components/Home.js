6182aea7a1c0819a4509c1ccbafc69a148cb860b
annomao <omaoannkerubo@gmail.com>
2022-06-26 23:03:53 +0300

    add home component details

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+
+function Home() {
+  return (
+    <>
+    <Header/>
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-7xl text-base my-4">IReport</h1>
+        <p className="my-4">Do you want to report corruption or tell us something that needs Government intervention?</p>
+        <a className="inline-block text-sm px-8 py-4 leading-none border text-white bg-base hover:border-transparent mt-4 lg:mt-0">Tell Us</a>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
