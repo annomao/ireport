397927e2c4df847bf23c152d7274901b5c306ce8
annomao <omaoannkerubo@gmail.com>
2022-06-26 22:10:13 +0300

    add header component and custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base' : '#157A6E' 
+      }
+    },
   },
   plugins: [],
 }
