53071fb5668db1fec4e17d8d8286d8d3b035cb53
annomao <omaoannkerubo@gmail.com>
2022-06-29 01:00:53 +0300

    add function to handle submit on report creation

--- src/components/dashboard/CreateReport.js
+++ src/components/dashboard/CreateReport.js
@@ -1,18 +1,37 @@
 import React, {useState} from 'react'
 
-function CreateReport() {
+function CreateReport({onAddReport}) {
 
   const [title, setTitle] = useState("")
   const [location, setLocation] = useState("")
   const [comment, setComment] = useState("")
   const [type, setType] = useState("")
 
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    fetch(`https://ireport-api.herokuapp.com/reports`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        location: location,
+        comment: comment,
+        type_id: type
+      }),
+    })
+      .then((r) => r.json())
+      .then((addedRecord) => onAddReport(addedRecord));
+  }
+
   return (
     <>
     <div className="flex items-center justify-center h-screen">
     <div className="px-8 py-6 mt-4 text-left bg-white shadow-2xl">
         <h3 className="text-2xl font-bold text-center text-base">Create Report</h3>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="mt-4">
             <label className="block text-base" htmlFor="title">Title</label>
             <input type="text" placeholder="title"
