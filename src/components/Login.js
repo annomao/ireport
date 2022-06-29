aa7ae476cf1ca239796aff737279f08dded46ca9
annomao <omaoannkerubo@gmail.com>
2022-06-30 00:31:06 +0300

    edit register and login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,50 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from './Header'
 
 function Login() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [errors, setErrors] = useState("")
+  const navigate = useNavigate()
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    fetch(`https://ireport-api.herokuapp.com/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+    })
+      .then((r) => {
+        if(r.status === 403){
+          setErrors("Incorrect email and/or password")
+          setEmail(""); setPassword("")
+        }
+        else{
+          r.json()
+        }
+      })
+      .then(data => {
+        localStorage.setItem("user", JSON.stringify(data))
+        navigate("/dashboard")
+        setEmail(""); setPassword("")
+      })
+  }
+
   return (
     <>
     <Header/>
     <div className="flex items-center justify-center h-screen">
     <div className="px-8 py-6 mt-4 text-left bg-white shadow-2xl">
         <h3 className="text-2xl font-bold text-center text-base">Login to your account</h3>
-        <form>
+        <form onSubmit={handleFormSubmit}>
+        <h3 className="font-bold text-lg text-red-600">{errors}</h3>
           <div className="mt-4">
             <label className="block text-base" htmlFor="email">Email</label>
             <input type="text" placeholder="Email"
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from './Header'
 
 function Register() {
@@ -6,6 +7,36 @@ function Register() {
   const[username, setUsername] = useState("")
   const[email, setEmail] = useState("")
   const[password,setPassword] = useState("")
+  const[errors, setErrors] = useState("")
+  const navigate = useNavigate();
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    fetch(`https://ireport-api.herokuapp.com/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        username: username,
+        email: email,
+        password: password
+      }),
+    })
+      .then((r) => {
+        if(r.status === 409){
+          setErrors("User already exists, try again or login to your account!")
+          setName(""); setEmail(""); setUsername(""); setPassword("")
+        }
+        else{
+          r.json()
+          setName(""); setEmail(""); setUsername(""); setPassword("")
+          navigate("/login")
+        }
+      })
+  }
 
   return (
     <>
@@ -13,7 +44,8 @@ function Register() {
     <div className="flex items-center justify-center h-screen">
     <div className="px-8 py-6 mt-4 text-left bg-white shadow-2xl">
         <h3 className="text-2xl font-bold text-base text-center">Register for an account</h3>
-        <form>
+        <form onSubmit={handleFormSubmit}>
+        <h3 className="font-bold text-lg text-red-600">{errors}</h3>
           <div className="mt-4">
             <label className="block text-base" htmlFor="name">Full Name</label>
             <input type="text" placeholder="Full name"
